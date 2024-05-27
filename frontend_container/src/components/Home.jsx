3afb1a71b87544452be38c0bb49986b468552354
Wencho8 <wenchoavalos8@gmail.com>
2024-05-27 19:45:32 -0300

    Front-end styling completed
     Differentiation between good and bad characters using color palettes, characters structured in rows, and re-styling of the inputs.

--- frontend_container/src/components/Home.jsx
+++ frontend_container/src/components/Home.jsx
@@ -76,52 +76,73 @@ function Home() {
 
     return (
         <div>
-            <h1 className="pt-6 text-center font-bold text-4xl p-3">Home</h1>
-            <div className="text-center text-2xl mb-4">
+            <div className="text-center text-4xl pt-6 mb-8">
                 <p>Bienvenido a esta página web creada con React y Docker</p>
             </div>
-            <h2 className="pt-6 text-center font-bold text-xl p-3">Insertar un personaje:</h2>
-            <form className="text-center" onSubmit={ submitHandler }>
-                <input 
-                    type='text'
-                    placeholder='Nombre del personaje...'
-                    value={enteredName}
-                    onChange={nameChangeHandler}
-                />
-                <input 
-                    type='text'
-                    placeholder='Película...'
-                    value={enteredFilm}
-                    onChange={filmChangeHandler}
-                />
-                <input 
-                    type='text'
-                    placeholder='Habilidad...'
-                    value={enteredSkill}
-                    onChange={skillChangeHandler}
-                />
-                <li>
-                    <input
-                        type='checkbox'
-                        value={enteredIsGood}
-                        onChange={isGoodChangeHandler}
-                    />
-                    <label>¿Es bueno?</label>
-                </li>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cargar personaje</button>
-            </form>
-            <div className="text-center text-xl mt-4">
-                <h2 className="font-semibold text-3xl mb-2">Personajes traídos desde la base de datos:</h2>
+            <div className="flex flex-col w-3/4 bg-zinc-300 rounded-lg p-4 mx-auto cursor-pointer transition duration-300 hover:scale-105 hover:shadow-lg hover:border-zinc-500 hover:shadow-zinc-700">
+                <h2 className=" text-center font-semibold text-3xl p-3 mb-4">Insertar un personaje</h2>
+                <form className="flex flex-col items-center space-y-4" onSubmit={submitHandler}>
+                    <div className="flex w-2/3 justify-center space-x-4">
+                        <input
+                            className="flex-grow w-1/5 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-500"
+                            type="text"
+                            placeholder="Nombre del personaje..."
+                            value={enteredName}
+                            onChange={nameChangeHandler}
+                        />
+                        <input
+                            className="flex-grow w-1/5 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-500"
+                            type="text"
+                            placeholder="Película..."
+                            value={enteredFilm}
+                            onChange={filmChangeHandler}
+                        />
+                        <input
+                            className="flex-grow w-1/5 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-500"
+                            type="text"
+                            placeholder="Habilidad..."
+                            value={enteredSkill}
+                            onChange={skillChangeHandler}
+                        />
+                    </div>
+                    <li className="flex items-center space-x-2">
+                        <input
+                            id="isGoodCheckbox"
+                            className="form-checkbox"
+                            type="checkbox"
+                            checked={enteredIsGood}
+                            onChange={isGoodChangeHandler}
+                        />
+                        <div className="relative inline-block">
+                            <label
+                                htmlFor="isGoodCheckbox"
+                                onClick={() => setIsGood(!enteredIsGood)}
+                                className={`text-gray-500 cursor-pointer italic font-bold relative after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-full after:h-0.5 ${enteredIsGood ? 'after:bg-green-700' : 'after:bg-red-700'} after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100`}
+                            >
+                                ¿Es bueno?
+                            </label>
+                        </div>
+                    </li>
+                    <button className="bg-zinc-600 hover:bg-zinc-800 text-white font-bold py-2 px-4 rounded">
+                        Cargar personaje
+                    </button>
+                </form>
+            </div>
+            <div className="text-center text-xl mt-20 mb-10">
+                <h2 className="font-semibold text-3xl mb-10">Personajes traídos desde la base de datos</h2>
                 {characters.length > 0 ? (
-                    <ul className="list-disc list-inside">
+                    <div className="flex flex-wrap justify-center gap-4">
                         {characters.map((character, index) => (
-                            <li key={index} className="mb-1">
-                                <b>Personaje:</b> { character.nombre } <b>Película:</b> { character.pelicula } <b>Habilidad:</b> { character.habilidad } <b>¿Es bueno?</b> { character.bueno ? 'Sí' : 'No' }
-                            </li>
+                            <div key={index} className={`bg-gray-200 rounded-lg p-4 cursor-pointer transition duration-300 hover:scale-105 hover:shadow-lg ${character.bueno ? 'hover:border-green-500 hover:shadow-green-700' : 'hover:border-red-500 hover:shadow-red-700'}`} style={{ minWidth: '400px' }}>
+                                <p className={`text-center mb-2 font-bold ${character.bueno ? 'text-green-700' : 'text-red-600'}`}>{character.nombre}</p>
+                                <p><b>Película:</b> {character.pelicula}</p>
+                                <p><b>Habilidad:</b> {character.habilidad}</p>
+                                <p className="text-center"><b>¿Es bueno?</b> {character.bueno ? <span className="italic text-green-700 font-semibold">Sí</span> : <span className="italic text-red-600 font-semibold">No</span>}</p>
+                            </div>
                         ))}
-                    </ul>
+                    </div>
                 ) : (
-                    <p>Cargando números...</p>
+                    <p>Buscando personajes...</p>
                 )}
             </div>
         </div>
--- frontend_container/src/components/Navbar.jsx
+++ frontend_container/src/components/Navbar.jsx
@@ -11,18 +11,18 @@ function Navbar() {
     };
 
     return (
-    <>
-        <div className="flex bg-zinc-700 p-6 items-center justify-between">
-            <h2 className="text-white font-normal text-2xl">
-                Software Libre 2024
-            </h2>
-            <div className={`z-50 absolute offScreen right-0 mr-auto ml-auto text-center ease-in-out md:static md:m-0 text-white ${clicked ? "w-full block top-1/3 left-0 mt-2" : ""}`}>
-                <Link onClick={handleClick} to="/" className={`mr-4 text-xl md:text-base md:text-white md:inline ${clicked ? "text-3xl mt-4" : ""}`}>
-                Home
-                </Link>
+        <>
+            <div className="flex bg-zinc-700 p-6 items-center justify-between">
+                <h2 className="text-white font-normal text-2xl cursor-pointer duration-300 transition-transform transform hover:scale-105">
+                    Software Libre 2024
+                </h2>
+                <div className={`z-50 absolute offScreen right-0 mr-auto ml-auto text-center ease-in-out md:static md:m-0 text-white cursor-pointer duration-300 transition-transform transform hover:scale-110 ${clicked ? "w-full block top-1/3 left-0 mt-2" : ""}`}>
+                    <Link onClick={handleClick} to="/" className={`mr-4 text-xl md:text-base md:text-white md:inline  ${clicked ? "text-3xl mt-4" : ""} `}>
+                        Home
+                    </Link>
+                </div>
             </div>
-        </div>
-    </>
+        </>
     );
 }
 
